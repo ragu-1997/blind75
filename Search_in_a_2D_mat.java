10fb34c6013b2a54b25776d5ecbcc29db0b5f737
Aayush Pandey <aayushpandey1100@gmail.com>
2022-10-12 18:38:43 +0530

    search in a 2d matrix

--- Search_in_a_2D_mat.java
+++ Search_in_a_2D_mat.java
@@ -25,49 +25,47 @@ public class Search_in_a_2D_mat {
 
 }
 
-/**
- * Solution:
- * 
- * Approach 1 (My approach)(O(n))
- * class Solution {
- * public boolean searchMatrix(int[][] arr, int target) {
- * int col=arr[0].length-1;
- * int row=arr.length-1;
- * int temp=-1;
- * for(int i=0;i<=row;i++){
- * if(arr[i][0]<=target && arr[i][col]>=target){
- * temp=i;
- * break;
- * }
- * }
- * if(temp!=-1){
- * for(int j=0;j<=col;j++){
- * if(arr[temp][j]==target){
- * return true;
- * }
- * }}
- * return false;
- * }
- * }
- * 
- * Approach 2 (Striver Bhaiya)O(log(n*m))
- * 
- * if(arr.length==0)return false;
- * int n=matrix.length;
- * int m=matrix[0].length;
- * int lo=0;
- * int hi=(n*m)-1;
- * while(lo<=high){
- * int mid=(lo+(hi-lo)/2);
- * if(matrix[mid/m][mid%m]==target){
- * return true;
- * }
- * if(matrix[mid/m][mid%m]<target){
- * lo=mid+1;
- * }else{
- * hi=mid-1;
- * }
- * }
- * return false;
- * }
- */
+Solution:
+
+// Approach 1 (My approach)(O(n))
+class Solution {
+ public boolean searchMatrix(int[][] arr, int target) {
+ int col=arr[0].length-1;
+ int row=arr.length-1;
+ int temp=-1;
+ for(int i=0;i<=row;i++){
+ if(arr[i][0]<=target && arr[i][col]>=target){
+ temp=i;
+ break;
+ }
+ }
+ if(temp!=-1){
+ for(int j=0;j<=col;j++){
+ if(arr[temp][j]==target){
+ return true;
+ }
+ }}
+ return false;
+ }
+ }
+ 
+ //Approach 2 (Striver Bhaiya)O(log(n*m))
+ 
+ if(arr.length==0)return false;
+ int n=matrix.length;
+ int m=matrix[0].length;
+ int lo=0;
+ int hi=(n*m)-1;
+ while(lo<=high){
+ int mid=(lo+(hi-lo)/2);
+ if(matrix[mid/m][mid%m]==target){
+ return true;
+ }
+ if(matrix[mid/m][mid%m]<target){
+ lo=mid+1;
+ }else{
+ hi=mid-1;
+ }
+ }
+ return false;
+ }
