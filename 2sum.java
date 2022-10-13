f52f36cfe8a796e2ab404096d56f35dd8bb685a5
SUSHIL PANDEY <95053082+codERSunny812@users.noreply.github.com>
2022-10-14 02:00:37 +0530

    updated two sum java

--- 2sum.java
+++ 2sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        ArrayList<Integer> l1 = new ArrayList<>();
-
-            for(int i=0;i<nums.length;i++)
-            {
-                    for(int j = i+1;j<nums.length;j++)
-                    {
-                            int value = nums[i]+nums[j];
-                            if(target==value)
-                            {
-                                    return new int[]{i,j};
-                            }
-                    }
+        int [] indices= new int[2];
+        
+        for(int i=0; i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]== target){
+                    indices[0]=i;
+                    indices[1]=j;
+                    break;
+                }
             }
-            return new int[]{0,0};
+    
+        }
+        return indices;
     }
+    
 }
